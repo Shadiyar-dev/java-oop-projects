dcd8de6e77c9d979c968451b48732e9098ed813f
Shadiyar-dev <shadiarturarbek@gmail.com>
2026-02-07 15:53:04 +0500

    Delete Modul-3/Домашнее-задание/ISP directory

--- Modul-3/Домашнее-задание/ISP/BasicPrinter.java
+++
@@ -1,8 +0,0 @@
-package isp;
-
-public class BasicPrinter implements Printer {
-    @Override
-    public void print(String content) {
-        System.out.println("Басып шығаруда: " + content);
-    }
-}
